c06e72f8a3ea75365ee74eda34ef4d3f2bb9cf40
Sanahas Kodisinghe <sanahas.kodisinghe@gmail.com>
2022-12-16 13:51:09 +0300

    Main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Signin from "./components/jsx/index.jsx";
+import Dashbaord from "./components/jsx/dashboard.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SideNav from "./components/jsx/sidenav";
+import "./App.css";
+import ForgotPass from "./components/jsx/forgotpass";
+import Profilepage from "./components/jsx/profilepage.jsx";
+import Profilepageadd from "./components/jsx/profilepageadd.jsx";
+import Registry from "./components/jsx/registry";
+import Registryinformation from "./components/jsx/registryinformation";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Signin />} />
+          <Route path="dashboard" element={<Dashbaord />} />
+          <Route path="forgot_password" element={<ForgotPass />} />
+          <Route path="profilepage" element={<Profilepage />} />
+          <Route path="profilepageadd" element={<Profilepageadd />} />
+          <Route path="registry" element={<Registry />} />
+          <Route path="registryinformation" element={<Registryinformation />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
